e05abbcc78964c107f71002cf069b000321161de
ogeeDeveloper <omarogrant202@hotmail.com>
2022-06-09 09:28:00 -0500

    Added the planets data on startup and used Promise to hold the starting of the server until the dta have completely retrieved

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -9,27 +9,47 @@ function isHabitablePlanet(planet) {
     && planet['koi_prad'] < 1.6;
 }
 
-fs.createReadStream('../../data/kepler_data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
-  .on('data', (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on('error', (err) => {
-    console.log(err);
-  })
-  .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
-    console.log(`${habitablePlanets.length} habitable planets found!`);
-  });
+/*
+--Creating a new Promise--
+const promise = new Promise((resolve, reject)=>{
+    resolve(100)
+})
+then call the promise like so:
+promise.then(result=>{
+
+})
+
+then await the result from the promise
+const result = wait promise
+
+NB: This promise will block our code from sending any other request before completing this execution
+*/
+const loadPlanetsData = async()=>{
+    // This promise is used to check and tell us when the planets have been succsfully loaded
+    return new Promise((resolve, reject)=>{
+        fs.createReadStream('../../data/kepler_data.csv')
+        .pipe(parse({
+            comment: '#',
+            columns: true,
+        }))
+        .on('data', (data) => {
+            if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err)
+        })
+        .on('end', () => {
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+            resolve()
+        });
+    })
+}
 
 
 module.exports = {
+    loadPlanetsData,
     planets: habitablePlanets,
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,17 @@
 const http = require("http")
 const app = require("./app")
+const { loadPlanetsData } = require("./models/planets.model")
 
 const PORT = process.env.PORT || 8000
 const server = http.createServer(app)
 
-server.listen(PORT, ()=>{
-    console.log(`Listeing on port ${PORT}...`)
-})
+const startServer = async()=>{
+    // This will load the data before the server starts listening and starts to response to request
+    await loadPlanetsData()
+
+    server.listen(PORT, ()=>{
+        console.log(`Listeing on port ${PORT}...`)
+    })
+}
+
+startServer()
