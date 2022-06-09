7fcf317f0d9076dff7d0e7d4f1a06c59bef3bd0a
ogeeDeveloper <omarogrant202@hotmail.com>
2022-06-09 08:30:24 -0500

    Added the CORS middleware to allow the frontend URL

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 
 //Import the plkanetsRouter to get all routes related to the planet route
 const planetRouter = require('./routes/planet/planets.router')
@@ -6,6 +7,9 @@ const planetRouter = require('./routes/planet/planets.router')
 // Request first comes in the app
 const app = express()
 
+app.use(cors({
+    origin:"http://localhost:3000",
+}))
 // Checks for JSON data
 app.use(express.json())
 //Handles all pllanets routes
