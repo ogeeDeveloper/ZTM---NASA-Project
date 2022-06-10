0061e42eabadc8aaf0b3b46845d433a84f2f78b7
ogeeDeveloper <omarogrant202@hotmail.com>
2022-06-10 08:44:53 -0500

    Created a production ready frontend by building it using npm build and serving it staticaly on one port using express

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 
 //Import the plkanetsRouter to get all routes related to the planet route
 const planetRouter = require('./routes/planet/planets.router')
@@ -12,7 +13,13 @@ app.use(cors({
 }))
 // Checks for JSON data
 app.use(express.json())
+// middleware to serve all our public files
+app.use(express.static(path.join(__dirname, '..', 'public')))
 //Handles all pllanets routes
 app.use(planetRouter)
+// set the "/" to load the launch route so that the user doesnt have to type index.html
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app
